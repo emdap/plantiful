835a8d01da6d4aa8d4e55c80ff35a7a2f19cc948
unknown <ecodapo@gmail.com>
2021-03-16 12:21:37 -0400

    added defaults + no verify on default plant response

--- src/fixtures/Grow/Defaults.ts
+++ src/fixtures/Grow/Defaults.ts
@@ -1,6 +1,61 @@
-import { GrowBasis } from "@/store/interfaces"
+import { GrowBasis, GrowBorder, GrowShape, Plant } from "@/store/interfaces"
 
 // const defaultGrowBasis: GrowBasis = {
 // position: "relative",
 // height
 // }
+
+export const triangleBorder: GrowBorder = {
+  top: {
+    size: 25,
+    show: false
+  },
+  right: {
+    size: 50,
+    show: true
+  },
+  bottom: {
+    size: 25,
+    show: false
+  }
+}
+
+export const triangleBasis: GrowBasis = {
+  position: {
+    bottom: 0
+  },
+  rotation: {
+    x: 0,
+    y: 0,
+    z: 0
+  },
+  height: 0,
+  width: 0
+}
+
+export const testPlant: Plant = {
+  id: 137442,
+  common_name: "common velvetgrass",
+  scientific_name: "Holcus lanatus",
+  main_species_id: 143075,
+  image_url:
+    "https://bs.plantnet.org/image/o/46619775d4319328b2fad6f1ba876ccca2d03534",
+  family_common_name: "Grass family",
+  family: "Poaceae",
+  main_species: {
+    id: 143075,
+    common_name: "Common velvetgrass",
+    scientific_name: "Holcus lanatus",
+    family_common_name: "Grass family",
+    image_url:
+      "https://bs.plantnet.org/image/o/46619775d4319328b2fad6f1ba876ccca2d03534",
+    flower: { color: ["purple"] },
+    foliage: { texture: "fine", color: ["grey"] },
+    specifications: {
+      average_height: { cm: 100 },
+      shape_and_orientation: "Erect"
+    },
+    growth: { spread: { cm: null } },
+    family: "Poaceae"
+  }
+}
