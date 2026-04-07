7b09bf6938c03bbe2652478dd925a40f467487e2
Emma DaPonte <ecodapo@gmail.com>
2026-04-07 16:23:02 -0700

    add back trefle API by forwarding requests through hotplants server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,39 +1,39 @@
-import { JWTResponse } from "@/store/interfaces"
-import { create, ApiResponse } from "apisauce"
-import { AxiosRequestConfig } from "axios"
+import { ApiResponse, create } from "apisauce"
 
-const PLANT_API = process.env.VUE_APP_PLANT_API
-const TOKEN_API = process.env.VUE_APP_TOKEN_API
-let JWT!: JWTResponse
+// const PLANT_API = process.env.VUE_APP_PLANT_API
+// const TOKEN_API = process.env.VUE_APP_TOKEN_API
+// let JWT!: JWTResponse
+
+export const API_VERSION = "/api/v1"
 
 const plantAPI = create({
-  baseURL: PLANT_API,
-  headers: { "content-type": "application/json" }
+  baseURL: `${process.env.VUE_APP_HOTPLANTS_URL}/trefle/${API_VERSION}`,
+  headers: { "content-type": "application/json" },
 })
 
-const tokenAPI = create({
-  baseURL: TOKEN_API,
-  headers: { "content-type": "application/json" }
-})
+// const tokenAPI = create({
+//   baseURL: TOKEN_API,
+//   headers: { "content-type": "application/json" },
+// })
 
-function expirationPassed(): boolean {
-  return JWT && new Date() > new Date(JWT.expiration)
-}
+// function expirationPassed(): boolean {
+//   return JWT && new Date() > new Date(JWT.expiration)
+// }
 
-plantAPI.addAsyncRequestTransform(async (request: AxiosRequestConfig) => {
-  // initialize token
-  if (!JWT || expirationPassed()) {
-    const tokenResponse = (await tokenAPI.get("/jwt")) as ApiResponse<
-      JWTResponse
-    >
-    if (tokenResponse.ok && tokenResponse.data) {
-      JWT = tokenResponse.data
-    } else {
-      throw tokenResponse.originalError
-    }
-  }
-  request.headers.Authorization = `Bearer ${JWT.token}`
-})
+// plantAPI.addAsyncRequestTransform(async (request: AxiosRequestConfig) => {
+//   // initialize token
+//   if (!JWT || expirationPassed()) {
+//     const tokenResponse = (await tokenAPI.get(
+//       "/jwt"
+//     )) as ApiResponse<JWTResponse>
+//     if (tokenResponse.ok && tokenResponse.data) {
+//       JWT = tokenResponse.data
+//     } else {
+//       throw tokenResponse.originalError
+//     }
+//   }
+//   request.headers.Authorization = `Bearer ${JWT.token}`
+// })
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export const resolve = <S, E>(response: ApiResponse<S, E>): any => {
--- src/services/plants.ts
+++ src/services/plants.ts
@@ -1,26 +1,26 @@
 import api, { resolve } from "@/services/api"
 import {
+  PlantListPayload,
   PlantListResponse,
   PlantResponse,
-  PlantListPayload
 } from "@/store/interfaces"
 
 export const listPlants = (
   payload: PlantListPayload
 ): Promise<PlantListResponse> =>
-  api.get(`/api/v1/plants?page=${payload.page}${payload.filter}`).then(resolve)
+  api.get(`/plants?page=${payload.page}${payload.filter}`).then(resolve)
 
 export const searchPlants = (
   payload: PlantListPayload
 ): Promise<PlantListResponse> =>
   api
     .get(
-      `/api/v1/plants/search?q=${payload.query}&page=${payload.page}${payload.filter}`
+      `/plants/search?q=${payload.query}&page=${payload.page}${payload.filter}`
     )
     .then(resolve)
 
 export const getPlant = (id: number): Promise<PlantResponse> =>
-  api.get(`api/v1/plants/${id}`).then(resolve)
+  api.get(`/plants/${id}`).then(resolve)
 
 export const getLink = <T>(link: string): Promise<T> =>
   api.get(link).then(resolve)
